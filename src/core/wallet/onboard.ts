e099c47acf2c9b8849ec06f3569a2f180c0f7b2c
Nejc Drobnic <ethoarx@gmail.com>
2022-05-03 14:34:52 +0200

    feat: support onto

--- src/core/wallet/onboard.ts
+++ src/core/wallet/onboard.ts
@@ -1,10 +1,34 @@
+import type { EIP1193Provider } from '@web3-onboard/common';
 import gnosisModule from '@web3-onboard/gnosis';
 import injectedModule from '@web3-onboard/injected-wallets';
+import { CustomWindow, InjectedNameSpace, InjectedWalletModule } from '@web3-onboard/injected-wallets/dist/types';
 import { init } from '@web3-onboard/react';
 import walletConnectModule from '@web3-onboard/walletconnect';
 import { HEX_CHAIN_IDS, RPC_URLS, SupportedChainId } from 'constants/chains';
 
-const injected = injectedModule();
+declare const window: CustomWindow;
+
+function getInjectedInterface(identity: string): () => Promise<{ provider: EIP1193Provider }> {
+	// eslint-disable-next-line @typescript-eslint/require-await
+	return async () => ({
+		provider: (window.ethereum.providers && Array.isArray(window.ethereum.providers)
+			? window.ethereum.providers.find((provider) => Boolean(provider[identity]))
+			: window.ethereum) as EIP1193Provider
+	});
+}
+
+const onto: InjectedWalletModule = {
+	label: 'Onto',
+	injectedNamespace: InjectedNameSpace.Ethereum,
+	checkProviderIdentity: ({ provider }) => Boolean(provider) && Boolean(provider.isONTO),
+	getIcon: async () => (await import('./icons/onto')).default,
+	getInterface: getInjectedInterface('isONTO'),
+	platforms: ['mobile']
+};
+
+const injected = injectedModule({
+	custom: [onto]
+});
 const gnosis = gnosisModule();
 const walletConnect = walletConnectModule({
 	bridge: 'https://bridge.walletconnect.org',
